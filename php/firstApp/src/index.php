0779a350a1d29b9715cf5ecb1d2d581e8a38985c
Manuel Idrobo <feridrobo3@gmail.com>
2023-03-02 17:55:46 -0500

    php-document

--- php/firstApp/src/index.php
+++ php/firstApp/src/index.php
@@ -7,7 +7,7 @@ $manager = new \MongoDB\Driver\Manager("mongodb://mongo:27017");
 $collection = "test_collection";
 
 // Documentos que se insertarán
-$doc1 = ['_id' => 1, 'name' => 'John Doe', 'age' => 35];
+$doc1 = ['_id' => 1, 'name' => 'John Doe', 'age' => 100];
 $doc2 = ['_id' => 2, 'name' => 'Jane Smith', 'age' => 28];
 
 // Eliminar los documentos existentes
@@ -21,6 +21,12 @@ $bulk->insert($doc1);
 $bulk->insert($doc2);
 $manager->executeBulkWrite("testdb.$collection", $bulk);
 
+
+// Modificar el documento con _id = 1
+$bulk = new \MongoDB\Driver\BulkWrite();
+$bulk->update(['_id' => 1], ['$set' => ['age' => 99]]);
+$manager->executeBulkWrite("testdb.$collection", $bulk);
+
 // Consulta para obtener los documentos insertados
 $filter = [];
 $options = [];
@@ -31,6 +37,27 @@ $result = $manager->executeQuery("testdb.$collection", $query);
 foreach ($result as $document) {
     var_dump($document);
 }
+$result = $manager->executeQuery("testdb.$collection", $query);
+// Mostrar los documentos obtenidos en una tabla
+echo "<table>";
+echo "<thead><tr><th>ID</th><th>Name</th><th>Age</th></tr></thead>";
+echo "<tbody>";
+foreach ($result as $document) {
+    echo "<tr>";
+    echo "<td>" . $document->_id . "</td>";
+    echo "<td>" . $document->name . "</td>";
+    echo "<td>" . $document->age . "</td>";
+    echo "</tr>";
+}
+echo "</tbody>";
+echo "</table>";
 
+$result = $manager->executeQuery("testdb.$collection", $query);
+
+
+// Mostrar los campos 'name' y 'age' de cada documento
+foreach ($result as $document) {
+    echo "Name: " . $document->name . ", Age: " . $document->age . "<br>";
+}
 
 ?>
