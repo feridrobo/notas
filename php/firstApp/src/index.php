4846df72647f0f0478b451f870831af7ab6e87c2
Manuel Idrobo <feridrobo3@gmail.com>
2023-03-02 16:06:58 -0500

    firs App

--- php/firstApp/src/index.php
+++ php/firstApp/src/index.php
@@ -1,5 +1,30 @@
 <?php
-require 'vendor/autoload.php';
-$suma = 4 + 6;
-echo "Hola, mundo! $suma";
+
+// Conexión a la base de datos
+$manager = new \MongoDB\Driver\Manager("mongodb://mongo:27017");
+
+// Colección en la que se insertarán los datos
+$collection = "test_collection";
+
+// Documentos que se insertarán
+$doc1 = ['_id' => 1, 'name' => 'John Doe', 'age' => 35];
+$doc2 = ['_id' => 2, 'name' => 'Jane Smith', 'age' => 28];
+
+// Insertar los documentos en la colección
+$bulk = new \MongoDB\Driver\BulkWrite();
+$bulk->insert($doc1);
+$bulk->insert($doc2);
+$manager->executeBulkWrite("testdb.$collection", $bulk);
+
+// Consulta para obtener los documentos insertados
+$filter = [];
+$options = [];
+$query = new \MongoDB\Driver\Query($filter, $options);
+$result = $manager->executeQuery("testdb.$collection", $query);
+
+// Mostrar los documentos obtenidos
+foreach ($result as $document) {
+    var_dump($document);
+}
+
 ?>
